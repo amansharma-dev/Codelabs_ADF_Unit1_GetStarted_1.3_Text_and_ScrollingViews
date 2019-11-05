777cbe5878da17bfcba20cc2c1529c6b463017f5
amansharma-dev <amansharma.11@hotmail.com>
2019-11-05 16:28:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/codelabs_adf_unit1_getstarted_13_text_and_scrollingviews/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.codelabs_adf_unit1_getstarted_13_text_and_scrollingviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
